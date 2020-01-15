1cd31a638fcb72299e75b2cb5e4771b30f9727be
Ryo Suzuki <ryo.s12330@gmail.com>
2020-01-15 20:08:06 +0900

    create comment function

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,12 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-
             <div class="card">
                 <div class="card-body">
                     <div>
                         <div>
-                        <h3>{{$content -> title}}</h3>
+                        <h3>{{$content->title}}</h3>
                         <a style="width:25%;" class="btn-flat-border" href="https://twitter.com/intent/tweet?url=http://13.231.161.57/{{$content->id}}&text={{$content->title}}/{{ $username }}&hashtags=laravel-ec" target="blank_">
                             Twiiterでシェアする
                         </a>
@@ -24,8 +23,42 @@
                                 <li>値段 : {{$content -> price}}円</li>
                             </ul>
                         </div>
-                        {{$content -> description}}
+                            {{$content -> description}}
                         </div>
+                </div>
+            </div>
+
+            <div style="margin: 10px;"><h4>ユーザーコメント</h4></div>
+
+            <div class="comments">
+                @if (count($comments) > 0)
+                    @foreach ($comments as $key => $comment)
+                        <div class="card">
+                            <div class="card-body">
+                                <div>
+                                    <h5>{{$comment->title}}</h5>
+                                    {{$comment->created_at->format('Y年m月d日 H時')}} {{$commentusernames[$key]}}さん<br>
+                                    {{$comment->content}}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+
+            <div class="card" style="margin-top: 10px;">
+                <div class="card-body">
+                    <div class="newcomment">
+                        <h4>コメントを投稿する</h4>
+                        <form action="/{{$content->id}}/newcomment" method="POST">
+                            @csrf
+                            コメントタイトル<br>
+                            <input size="60" name="commenttitle" placeholder="タイトルを入力"><br>
+                            コメント内容<br>
+                            <textarea cols="60" rows="4" name="comment" placeholder="コメントを入力"></textarea>
+                            <input type="submit">
+                        </form>
+                    </div>
                 </div>
             </div>
 
